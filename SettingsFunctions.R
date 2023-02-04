172478088cb443a800689aa29694bfc41de91656
Anthony Sena <asena5@its.jnj.com>
2023-02-04 09:25:05 -0500

    Add aws.s3 dependency for DatabaseConnector

--- SettingsFunctions.R
+++ SettingsFunctions.R
@@ -34,7 +34,7 @@ createCohortGeneratorModuleSpecifications <- function(incremental = TRUE,
   }
   
   specifications <- list(module = "CohortGeneratorModule",
-                         version = "0.0.14",
+                         version = "0.0.15",
                          remoteRepo = "github.com",
                          remoteUsername = "ohdsi",
                          settings = analysis)
