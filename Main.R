6c44e52e021b42a06d7131b30dd4e83bfb7254d1
Anthony Sena <anthonysena@users.noreply.github.com>
2023-09-01 08:26:45 -0400

    Add results data model creation and update renv.lock (#35)

--- Main.R
+++ Main.R
@@ -194,6 +194,33 @@ execute <- function(jobContext) {
   rlang::inform(paste("Results available at:", zipFile))
 }
 
+createDataModelSchema <- function(jobContext) {
+  checkmate::assert_class(jobContext$moduleExecutionSettings$resultsConnectionDetails, "ConnectionDetails")
+  checkmate::assert_string(jobContext$moduleExecutionSettings$resultsDatabaseSchema)
+  connectionDetails <- jobContext$moduleExecutionSettings$resultsConnectionDetails
+  moduleInfo <- getModuleInfo()
+  tablePrefix <- moduleInfo$TablePrefix
+  resultsDatabaseSchema <- jobContext$moduleExecutionSettings$resultsDatabaseSchema
+  resultsDataModel <- ResultModelManager::loadResultsDataModelSpecifications(
+    filePath = "resultsDataModelSpecification.csv"
+  )
+  resultsDataModel$tableName <- paste0(tablePrefix, resultsDataModel$tableName)
+  sql <- ResultModelManager::generateSqlSchema(
+    schemaDefinition = resultsDataModel
+  )
+  sql <- SqlRender::render(
+    sql = sql,
+    database_schema = resultsDatabaseSchema
+  )
+  connection <- DatabaseConnector::connect(
+    connectionDetails = connectionDetails
+  )
+  on.exit(DatabaseConnector::disconnect(connection))
+  DatabaseConnector::executeSql(
+    connection = connection,
+    sql = sql
+  )
+}
 
 # Private methods -------------------------
 getModuleInfo <- function() {
