0a4b3d4244406a282f9bf27e18be57ff385cf414
Anthony Sena <asena5@its.jnj.com>
2024-01-22 14:29:47 -0500

    Bump RMM ref and module ref

--- SettingsFunctions.R
+++ SettingsFunctions.R
@@ -35,7 +35,7 @@ createCohortGeneratorModuleSpecifications <- function(incremental = TRUE,
 
   specifications <- list(
     module = "CohortGeneratorModule",
-    version = "0.2.2-3",
+    version = "0.2.2-4",
     remoteRepo = "github.com",
     remoteUsername = "ohdsi",
     settings = analysis
