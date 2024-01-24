dde1f18bd4bdcb7061c29d5a58829a1380a2ce53
Anthony Sena <asena5@its.jnj.com>
2024-01-24 16:07:54 -0500

    Bump version for testing

--- SettingsFunctions.R
+++ SettingsFunctions.R
@@ -35,7 +35,7 @@ createCohortGeneratorModuleSpecifications <- function(incremental = TRUE,
 
   specifications <- list(
     module = "CohortGeneratorModule",
-    version = "0.2.2-6",
+    version = "0.2.2-7",
     remoteRepo = "github.com",
     remoteUsername = "ohdsi",
     settings = analysis
